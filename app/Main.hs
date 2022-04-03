73d7e0688ed77d3d13c5aa79bdc2fbe59c7d2dca
Xavier Shay <contact@xaviershay.com>
2022-04-04 08:17:19 +1000

    Add timings and fix up dependency config

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import Plants.SVG
-import Control.Monad (forM_)
-import Control.Lens (set)
-import Linear (V2(..))
 
 import Systems.Geometric
 import Systems.Plants2D
 import Systems.Penrose
+
+import Control.Monad (forM_)
+import Control.Lens (set)
+import Data.Time as T
+import Linear (V2(..))
 
 main :: IO ()
 main = do
@@ -25,22 +27,22 @@ renderGeometrics = do
         , ("gosper-hex-curve", gosperHexCurve)
         ]
   forM_ systems $ \(name, system) -> do
-    renderSvgToFile (set settingOutputDir "output/geometric-" default2d) system name
+    renderSvgWithTime (set settingOutputDir "output/geometric-" default2d) system name
 
 renderPlants2D = do
   let systems = map (\n -> ("branching-" <> show n, branching n)) [1..6]
   forM_ systems $ \(name, system) -> do
-    renderSvgToFile (set settingOutputDir "output/plants-2d-" default2d) system name
+    renderSvgWithTime (set settingOutputDir "output/plants-2d-" default2d) system name
 
 renderPenrose = do
-  renderSvgToFile (
+  renderSvgWithTime (
     set settingViewport (ViewportFixed (V2 (-5) (-5), V2 5 5))
     . set settingStrokeWidth 0.03
     $ default2d
     ) penroseStencil "penrose-stencil"
 
   -- Colors from https://coolors.co/ef476f-ffd166-b4e7f8-7cd5f3-e9e9ed
-  renderSvgToFile  (
+  renderSvgWithTime  (
     set settingViewport (ViewportBoundingRect 0.05)
     . set settingStrokeWidth 0.01
       . set settingBackground "#7CD5F3"
@@ -51,3 +53,10 @@ renderPenrose = do
         ]
     $ default2d)
     penrose "penrose"
+
+renderSvgWithTime settings system name = do
+  putStr name
+  start <- T.getCurrentTime
+  renderSvgToFile settings system name
+  finish <- T.getCurrentTime
+  putStrLn $ "\t (" <> show (T.diffUTCTime finish start) <> ")"
