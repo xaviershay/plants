242939998ceaf8e73209d7e23f360f95497ba109
Xavier Shay <contact@xaviershay.com>
2022-04-03 15:43:31 +1000

    Backfill parametric test cases, and missing guard code and fix multiparam.

--- test/LSystem.hs
+++ test/LSystem.hs
@@ -61,9 +61,7 @@ test_run =
             n 1
             axiom "Fa [ + Fa ] Fa [ - Fa ] Fb"
             productions [("Fa" |> "Fb", "Fb")]
-        , testSystem
-            "Parametric basipetal propagation"
-            "Fb(1) [ Fa(2) ] Fb(1)" $
+        , testSystem "Parametric basipetal propagation" "Fb(1) [ Fa(2) ] Fb(1)" $
           lsystem $ do
             n 1
             axiom "Fa(0) [ Fa(2) ] Fb(1)"
@@ -76,5 +74,59 @@ test_run =
         --     axiom "Fa(0) [ Fb(2) ] Fb(1)"
         --     productions [("Fa(x)" |> "Fb(y)" |: "y < 2", "Fb(y)")]
         ]
+    , testGroup
+        "Parametric"
+        [ testSystem "Basic substitution" "G(1, 2)" $
+          lsystem $ do
+            n 1
+            axiom "F(1, 2)"
+            productions [("F(x, y)", "G(x, y)")]
+        , testSystem "Addition" "F(3)" $
+          lsystem $ do
+            n 2
+            axiom "F(1)"
+            productions [("F(x)", "F(x+1)")]
+        , testSystem "Arithmetic" "F(4)" $
+          lsystem $ do
+            n 1
+            axiom "F(1)"
+            productions [("F(x)", "F((x*(x+9)/5)^2)")]
+        , testSystem "Defines" "F(4)" $
+          lsystem $ do
+            n 2
+            axiom "F(1)"
+            define [("y", "x*2")]
+            productions [("F(x)", "F(y)")]
+        , testSystem "Pre-conditions" "a a b(2)" $
+          lsystem $ do
+            n 2
+            axiom "b(2) a a"
+            productions [("b", "a"), ("b(x)" <| "a", "b(x)")]
+        , testSystem "Post-conditions" "b(2) a a" $
+          lsystem $ do
+            n 2
+            axiom "a a b(2)"
+            productions [("b", "a"), ("a" |> "b(x)", "b(x)")]
+        , testSystem "Guards" "b a(2) c(3) d(4)" $
+          lsystem $ do
+            n 2
+            axiom "a(1) a(2) a(3) a(4)"
+            productions
+              [ ("a(x)" |: "x < 2", "b")
+              , ("a(x)" |: "x = 3", "c(x)")
+              , ("a(x)" |: "x >= 4", "d(x)")
+              ]
+        , testSystem "Guards with defines" "b a(2) c(3)" $
+          lsystem $ do
+            n 2
+            axiom "a(1) a(2) a(3)"
+            define [("y", "3")]
+            productions
+              [("a(x)" |: "x <= 1", "b"), ("a(x)" |: "x + y > 5", "c(x)")]
+        , testSystem "Guards take precedence" "b c(2)" $
+          lsystem $ do
+            n 2
+            axiom "a(1) a(2)"
+            productions [("a(x)", "c(x)"), ("a(x)" |: "x <= 1", "b")]
+        ]
     ]
-    -- TODO: Verify that context-sensitive matches over context free (p. 30)
